6602a7517dbb216b079718824f17be3c1c5b8a09
Brad Sickles <brad.sickles@gmail.com>
2023-03-29 15:37:41 -0400

    Adding missing locals.

--- nullstone.tf
+++ nullstone.tf
@@ -19,6 +19,20 @@ resource "random_string" "resource_suffix" {
 
 locals {
   tags          = data.ns_workspace.this.tags
+  stack_name    = data.ns_workspace.this.stack_name
   block_name    = data.ns_workspace.this.block_name
+  env_name      = data.ns_workspace.this.env_name
   resource_name = "${data.ns_workspace.this.block_ref}-${random_string.resource_suffix.result}"
+
+  labels = {
+    // k8s-recommended labels
+    "app.kubernetes.io/name"       = local.block_name
+    "app.kubernetes.io/part-of"    = local.stack_name
+    "app.kubernetes.io/managed-by" = "nullstone"
+    // nullstone labels
+    "nullstone.io/stack"           = local.stack_name
+    "nullstone.io/block"           = local.block_name
+    "nullstone.io/env"             = local.env_name
+    "nullstone.io/ref"             = data.ns_workspace.this.block_ref
+  }
 }
