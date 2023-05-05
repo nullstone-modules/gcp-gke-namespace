dae85fa20cfcfc4ebc0069af7a1fbee49e3f6be2
Brad Sickles <brad.sickles@gmail.com>
2023-05-05 10:17:35 -0400

    Ensure external secrets depends on the namespace.

--- namespace.tf
+++ namespace.tf
@@ -2,7 +2,7 @@ locals {
   k8s_namespace = local.env_name
 }
 
-resource "kubernetes_namespace" "this" {
+resource "kubernetes_namespace_v1" "this" {
   metadata {
     name   = local.k8s_namespace
     labels = local.labels
